61bcfe98bb71e6d891ad8b77290c9f67fadf0bda
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-12-04 11:37:14 -0300

    animação

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+function proximo(){
+    document.getElementById("dep0").style.display = 'none';
+    document.getElementById("dep1").style.display = 'block';
+}
+
+function voltar(){
+    document.getElementById('dep1').style.display = 'none';
+    document.getElementById("dep0").style.display = 'block';
+}
